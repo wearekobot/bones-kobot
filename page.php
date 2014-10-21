8c06cabe8b691720e6d909f67e51a57048e6d3e6
Stefan Duret <sduret@gmail.com>
2014-10-21 11:26:04 -0400

    page.php is not a blog post

--- page.php
+++ page.php
@@ -1,40 +1,16 @@
 <?php get_header(); ?>
 			<div id="content">
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-							<header class="article-header">
-								<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-								<p class="byline vcard"><?php
-									printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( __( 'F jS, Y', 'bonestheme' ) ), bones_get_the_author_posts_link());
-								?></p>
-							</header>
-							
-							<section class="entry-content" itemprop="articleBody">
-								<?php the_content(); ?>
-							</section>
-							
-							<footer class="article-footer">
-								<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
-							</footer>
-							
-							<?php // comments_template(); ?>
-							
-						</article>
-						<?php endwhile; else : ?>
-						<article id="post-not-found" class="hentry">
-							<header class="article-header">
-								<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-							</header>
-							
-							<section class="entry-content">
-								<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-							</section>
-							
-							<footer class="article-footer">
-								<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
-							</footer>
-						</article>
-						<?php endif; ?>
-					<?php // get_sidebar(); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<section id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<h1 class="page-title"><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				</section>
+				<?php endwhile; else : ?>
+				<section id="post-not-found" class="hentry">
+					<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+					<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+				</section>
+				<?php endif; ?>
+				<?php // get_sidebar(); ?>
 			</div>
 <?php get_footer(); ?>
