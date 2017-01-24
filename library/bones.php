ef7de6b6eb2591ecc8b24fcb67de2796eb256112
Stefan Duret <sduret@gmail.com>
2017-01-24 10:34:08 -0700

    get rid of tinymce wpautop hack

--- library/bones.php
+++ library/bones.php
@@ -362,10 +362,3 @@ function bones_get_the_author_posts_link() {
 	);
 	return $link;
 }
-
-function my_format_TinyMCE($in) {
-	$in['remove_linebreaks'] = false;
-	$in['wpautop'] = false;
-	return $in;
-}
-add_filter('tiny_mce_before_init', 'my_format_TinyMCE');
