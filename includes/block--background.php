dcb4951c7514d5c4a5cb7892371de0922694ce77
Stefan Duret <sduret@gmail.com>
2018-02-28 16:07:33 -0700

    just gonna revert that back to BK’s version, I was dome and commited a thing before pulling in updates

--- includes/block--background.php
+++ includes/block--background.php
@@ -11,30 +11,28 @@
 	$backgroundImageDesktop = wp_get_attachment_image_src($image_id, 'hero-desktop');
 	$backgroundImageDesktopLarge = wp_get_attachment_image_src($image_id, 'hero-desktop-large');  
 ?>
-<?php if (has_post_thumbnail()) : ?>
 <style type="text/css" media="screen">
-	#container #intro-block{
+	#container #intro-block:not(.leader-default){
 		background-image: url(<?php echo $backgroundImageMobile[0]; ?>);
 	}
 	@media (min-width: 768px) and (max-width: 1024px) {
-		#container #intro-block{
+		#container #intro-block:not(.leader-default){
 			background-image: url(<?php echo $backgroundImageIpad[0]; ?>);
 		}
 	}
 	@media (min-width: 1025px) {
-		#container #intro-block{
+		#container #intro-block:not(.leader-default){
 			background-image: url(<?php echo $backgroundImageDesktopSmall[0]; ?>);
-		}
+		}	
 	}
 	@media (min-width: 1441px) {
-		#container #intro-block{
+		#container #intro-block:not(.leader-default){
 			background-image: url(<?php echo $backgroundImageDesktop[0]; ?>);
-		}
+		}	
 	}
 	@media (min-width: 2000px) {
-		#container #intro-block{
+		#container #intro-block:not(.leader-default){
 			background-image: url(<?php echo $backgroundImageDesktopLarge[0]; ?>);
-		}
+		}	
 	}
 </style>
-<?php endif; ?>
