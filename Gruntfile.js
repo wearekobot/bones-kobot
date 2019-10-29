29a5f144f3d7b201cecbbd444d3402007509e870
Stefan Duret <stefan@kobot.ca>
2019-10-29 10:53:16 -0600

    updated all npm packages, move to new .browserslistrc style

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
         map: false, // inline sourcemaps
         
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          require('autoprefixer')(), // add vendor prefixes
           require('cssnano')() // minify the result
         ]
       },
