5a7c30441311397b1cf1dc6a3db5c8047b93edcc
Stefan Duret <stefan@kobot.ca>
2020-03-31 14:33:34 -0600

    check for existence

--- includes/block--background.php
+++ includes/block--background.php
@@ -1,17 +1,19 @@
 <?php 
 
-	$placeholder_image = get_theme_mod('placeholder_image');
+$placeholder_image = get_theme_mod('placeholder_image');
 
-	if (has_post_thumbnail()) {
-		$image_id = get_post_thumbnail_id();
-	} elseif ((isset($placeholder_image) && !empty($placeholder_image))) {
-		$image_id = bones_get_attachment_id($placeholder_image);
-	}	
+if (has_post_thumbnail()) {
+	$image_id = get_post_thumbnail_id();
+} elseif ((isset($placeholder_image) && !empty($placeholder_image))) {
+	$image_id = bones_get_attachment_id($placeholder_image);
+}
+
+if (isset($image_id)) :
 	$backgroundImageMobile = wp_get_attachment_image_src($image_id, 'hero-mobile');
 	$backgroundImageIpad = wp_get_attachment_image_src($image_id, 'hero-ipad');
 	$backgroundImageDesktopSmall = wp_get_attachment_image_src($image_id, 'hero-desktop-small');
 	$backgroundImageDesktop = wp_get_attachment_image_src($image_id, 'hero-desktop');
-	$backgroundImageDesktopLarge = wp_get_attachment_image_src($image_id, 'hero-desktop-large');  
+	$backgroundImageDesktopLarge = wp_get_attachment_image_src($image_id, 'hero-desktop-large');
 ?>
 <style type="text/css" media="screen">
 	#container #intro-block{
@@ -25,16 +27,17 @@
 	@media (min-width: 1025px) {
 		#container #intro-block{
 			background-image: url(<?php echo $backgroundImageDesktopSmall[0]; ?>);
-		}	
+		}
 	}
 	@media (min-width: 1441px) {
 		#container #intro-block{
 			background-image: url(<?php echo $backgroundImageDesktop[0]; ?>);
-		}	
+		}
 	}
 	@media (min-width: 2000px) {
 		#container #intro-block{
 			background-image: url(<?php echo $backgroundImageDesktopLarge[0]; ?>);
-		}	
+		}
 	}
 </style>
+<?php endif; ?>
